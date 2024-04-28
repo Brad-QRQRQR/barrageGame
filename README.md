89e61382116a55800350bf6bb400159d2c2eeae3
林子堯_QRQRQR <69746364+Brad-QRQRQR@users.noreply.github.com>
2024-04-28 22:47:30 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-高二時以python + pygame + lmms + sfxr寫成的彈幕遊戲專案
+高二時以python + pygame + lmms + sfxr寫成的彈幕遊戲專案，並以pyinstaller打包為exe檔
--- background_tile3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="background_tile3" tilewidth="16" tileheight="17" tilecount="1" columns="1">
- <image source="images/background_tile3.png" width="16" height="17"/>
-</tileset>
